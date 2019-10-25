9adaa1e3a5c1de39e6fe2514d1c8796b762f6c56
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2019-10-25 01:24:10 -0400

    Versão 1.0 do script de backup customizado.

---
+++ backup_custom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Script para Backup com comando TAR e compactadores.
+echo
+read -p "IFORME O NOME PARA O ARQUIVO DE BACKUP: " namefile
+echo
+read -p "INFORME UM DIRETÓRIO PARA FAZER O BACKUP: " origem
+echo
+read -p "INFORME UM DESTINO PARA SALVAR O BACKUP: " destino
+dia=$(date +%d%m%y)
+arquivo="$namefile-$dia.tar.gz"
+dialog --title "BACKUP DE DADOS" --infobox "INICIANDO BACKUP!" 10 60; sleep 2
+dialog --title "BACKUP DE DADOS" --prgbox "tar -zcvpf $destino/$arquivo $origem" 30 120
+dialog --title "BACKUP CONCLUÍDO" --prgbox "ls -lh /mnt" 20 75
