7f29b2bed56cb3b43512df1a9b43b30c42b9b62f
Rafael Y. De Vidis <rafael.ydv@gmail.com>
2021-01-27 02:13:51 -0400

    Removido a compactação dos dados.

--- backup_custom.sh
+++ backup_custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script para Backup com comando TAR e compactadores.
+#Script para Backup com comando TAR sem compactadores.
 echo
 read -p "IFORME O NOME PARA O ARQUIVO DE BACKUP: " namefile
 echo
@@ -7,7 +7,7 @@ read -p "INFORME UM DIRETÓRIO PARA FAZER O BACKUP: " origem
 echo
 read -p "INFORME UM DESTINO PARA SALVAR O BACKUP: " destino
 dia=$(date +%d%m%y)
-arquivo="$namefile-$dia.tar.gz"
+arquivo="$namefile-$dia.tar"
 dialog --title "BACKUP DE DADOS" --infobox "INICIANDO BACKUP!" 10 60; sleep 2
-dialog --title "BACKUP DE DADOS" --prgbox "tar -zcvpf $destino/$arquivo $origem" 30 120
+dialog --title "BACKUP DE DADOS" --prgbox "tar -cvpf $destino/$arquivo $origem" 30 120
 dialog --title "BACKUP CONCLUÍDO" --prgbox "ls -lh /mnt" 20 75
